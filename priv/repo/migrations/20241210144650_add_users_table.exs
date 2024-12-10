bdad8d0e4b4d4e0959a3ffc9a72c8a943c721f7f
Emerson Bispo Ferreira <emerson.b.f02@outlook.com>
2024-12-10 12:18:07 -0300

    add user table

---
+++ priv/repo/migrations/20241210144650_add_users_table.exs
@@ -0,0 +1,13 @@
+defmodule BasicApi.Repo.Migrations.AddUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :password_hash, :string
+      add: enabled, :string
+
+      timestamps()
+  end
+end
