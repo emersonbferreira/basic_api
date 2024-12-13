dc854d2109fc324968fa1c74da029bbe922b5cb9
Emerson Bispo Ferreira <emerson.b.f02@outlook.com>
2024-12-13 08:39:45 -0300

    fix models_test

--- test/basic_api/models_test.exs
+++ test/basic_api/models_test.exs
@@ -6,27 +6,20 @@ defmodule BasicApi.ModelsTest do
   describe "users" do
     alias BasicApi.Models.User
 
-    import BasicApi.ModelsFixtures
-
     @invalid_attrs %{enabled: nil, name: nil, email: nil, password_hash: nil}
 
     test "create_user/1 with valid data creates a user" do
-      valid_attrs = %{enabled: true, name: "some name", email: "some email", password_hash: "some password_hash"}
+      valid_attrs = %{enabled: true, name: "some name", email: "some@email", password: "some password"}
 
       assert {:ok, %User{} = user} = Models.create_user(valid_attrs)
       assert user.enabled == true
       assert user.name == "some name"
-      assert user.email == "some email"
-      assert user.password_hash == "some password_hash"
+      assert user.email == "some@email"
+      assert Bcrypt.verify_pass("some password", user.password_hash)
     end
 
     test "create_user/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Models.create_user(@invalid_attrs)
-    end
-
-    test "change_user/1 returns a user changeset" do
-      user = user_fixture()
-      assert %Ecto.Changeset{} = Models.change_user(user)
     end
   end
 end
