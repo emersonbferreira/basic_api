54385350999f5b5ce7ea86c7725d5a82286e7dc5
Emerson Bispo Ferreira <emerson.b.f02@outlook.com>
2024-12-13 14:32:30 -0300

    add email_sender test

---
+++ test/basic_api/services/email_sender_test.exs
@@ -0,0 +1,36 @@
+defmodule BasicApi.Services.EmailSenderTest do
+  use ExUnit.Case, async: true
+  use BasicApi.DataCase
+
+  import Mock
+  import BasicApi.ModelsFixtures
+  
+  alias BasicApi.Services.EmailSender
+  alias BasicApi.Mailer
+  describe "notify_users" do
+    test "notify_users sends notifications to users with Hikvision cameras" do
+      user_1 = user_fixture(%{email: "email_1@teste"})
+      user_2 = user_fixture(%{email: "email_2@teste"})
+      camera_fixture(user_1)
+      camera_fixture(user_2)
+
+      email_1 = build_email(user_1)
+      email_2 = build_email(user_2)
+
+      with_mock Mailer, [deliver: fn _ -> :ok end] do
+      EmailSender.notify_users()
+      :timer.sleep(500)
+
+      assert called Mailer.deliver(email_1)
+      assert called Mailer.deliver(email_2)
+    end
+  end
+
+  defp build_email(user) do
+    Swoosh.Email.new()
+    |> Swoosh.Email.to(user.email)
+    |> Swoosh.Email.from("no-reply@basicapi.com")
+    |> Swoosh.Email.subject("Notification for Hikvision cams")
+    |> Swoosh.Email.html_body("<h1>Notification for Hikvision cams</h1>")
+  end
+end
